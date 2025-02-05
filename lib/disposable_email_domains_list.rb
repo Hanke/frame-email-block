deb71c6c08574f564a6987db0980021d2ff83e49
Cole Hanke <cjs_hanke@hotmail.com>
2025-02-05 17:25:52 -0600

    changed thingsgit commit -m "Update domains file path resolution method"

--- lib/disposable_email_domains_list.rb
+++ lib/disposable_email_domains_list.rb
@@ -16,7 +16,7 @@ module DisposableEmailDomainList
     private
 
     def domains_file
-      File.expand_path('./lib/disposable_email_domains/domains.json', __dir__)
+      File.expand_path('../disposable_email_domains/domains.json', __FILE__)
     end
   end
 end
